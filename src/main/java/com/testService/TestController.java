2bed56d9d09ca1b22a5f79fe8c6a560e62547c6f
Cristiane martins <cristianemartinsgmail@MacBook-Air-2.local>
2019-08-03 14:22:21 -0300

    Primeira alteracao

--- src/main/java/com/testService/TestController.java
+++ src/main/java/com/testService/TestController.java
@@ -3,7 +3,9 @@ package com.testService;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+/***
+* This is the javadoc example.
+*/
 @RestController
 @CrossOrigin
 public class TestController {
--- src/main/java/com/testService/TestServiceApplication.java
+++ src/main/java/com/testService/TestServiceApplication.java
@@ -1,21 +1,29 @@
 package com.testService;
 
-import com.testService.TestController;
+//import com.testService.TestController;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+/***
+* This is the javadoc example.
+*/
 @SpringBootApplication
 @ComponentScan(basePackageClasses = { TestController.class })
 @EnableAutoConfiguration
 public class TestServiceApplication {
-
-	private String bar = new String("bar");
-	public static void main(String[] args) {
-		SpringApplication.run(TestServiceApplication.class, args);
-		System.exit(0);
-	}
+/***
+* This is the javadoc example.
+*/
+    private String bar = new String("bar");
+/***
+* This is the javadoc example.
+*/
+    public static void main(String[] args) {
+        SpringApplication.run(TestServiceApplication.class, args);
+        System.exit(0);
+    }
 
 }
